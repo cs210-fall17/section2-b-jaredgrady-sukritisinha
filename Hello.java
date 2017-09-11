9a3be8d1150724111ac402cb6bc2fcc73712c7cd
thefenderstory <jgrady15@brophybroncos.org>
2017-09-11 09:59:50 -0700

    Make merge conflict

--- Hello.java
+++ Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 
     public static void callSign() {
 
-        system.out.println("Lock S-foils in attack positions");// Red Leader should add a print statement that says "Lock S-foils in attack position."
+        //system.out.println("Lock S-foils in attack positions");// Red Leader should add a print statement that says "Lock S-foils in attack position."
         system.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");// Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
