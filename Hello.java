335800465b4af8be158b18ca784afd5719312c82
thefenderstory <jgrady15@brophybroncos.org>
2017-09-11 10:04:51 -0700

    Fix System capitalization

--- Hello.java
+++ Hello.java
@@ -10,20 +10,21 @@ public class Hello {
 
     public static void callSign() {
 
-        system.out.println("Lock S-foils in attack positions");// Red Leader should add a print statement that says "Lock S-foils in attack position."
-        system.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");// Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
+        System.out.println("Lock S-foils in attack positions");
+        // Red Leader should add a print statement that says "Lock S-foils in attack position."
+        System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
+        // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
 
     public static void goldLeader() {
-
         // Gold Leader should add a print statement that says "Loosen up!"
-        system.out.println("Loosen up!");
+        System.out.println("Loosen up!");
     }
 
     public static void redLeader() {
-
-        system.out.println("I copy, gold leader");// Red Leader should add a print statement that says "I copy, gold leader!"
+        System.out.println("I copy, gold leader");
+        // Red Leader should add a print statement that says "I copy, gold leader!"
 
 
     }
